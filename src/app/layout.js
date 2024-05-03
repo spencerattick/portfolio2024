c17fe498064c957f3b92711ad5a51e86a2e58bf7
Spencer Attick <spencer.attick@gmail.com>
2024-05-03 16:37:29 -0400

    persist Navbar across all pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Intro from "@/components/Intro";
-import Navbar from "@/components/Navbar";
 
 const HomePage = () => {
   return (
       <>
-        <Navbar />
         <Intro />
       </>
   )
