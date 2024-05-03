635c7469097c752e3a0524db13cc448f5becc4ce
Spencer Attick <spencer.attick@gmail.com>
2024-05-03 14:50:52 -0400

    add intro content and begin styling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,14 +3,19 @@ import mainPhoto from '../../public/mainPhoto.png';
 
 const Intro = () => {
     return (
-        <div className='flex-col p-4'>
-            <h1 className='bg-black text-teal-600 font-bold text-7xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 p-5 text-center'>Spencer Attick</h1>
-            <h3 className='bg-black text-teal-600 font-bold text-3xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 p-5 text-center mx-10'>he/him</h3> 
-            <br />
-            <h2 className='bg-black text-teal-600 font-bold text-4xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 p-5 text-center'>Software Engineer</h2> 
-            {/* <Image src={mainPhoto} alt=''/> */}
-        </div>
-    )
+        <div className='flex justify-center items-center h-screen relative'>
+            <div className='absolute inset-0 z-0'>
+                <Image src={mainPhoto}  objectFit="cover" width={1700} alt="Main Photo" />
+            </div>
+
+            <div className='max-w-md relative z-10 mt-[-350px] flex flex-wrap justify-center'>
+                <h1 className='bg-black text-teal-600 font-bold text-8xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 p-5 text-center border-transparent text-nowrap'>Spencer Attick</h1>
+                <h3 className='bg-black text-teal-600 font-bold text-2xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 px-0 py-5 text-center mx-32 mt-[-20px] border-transparent'>he/him</h3> 
+                <br />
+                <h2 className='bg-black text-teal-600 font-bold text-3xl border border-r-red-500 border-b-red-500 border-r-4 border-b-4 p-5 text-center border-transparent'>Software Engineer</h2> 
+            </div>
+        </div> 
+    );
 }
 
 export default Intro;
