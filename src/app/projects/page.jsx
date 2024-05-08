f9eafd284fa2f796fcdf448111ded7d3e67a8172
Spencer Attick <spencer.attick@gmail.com>
2024-05-08 14:39:11 -0400

    start to add server-side fetch

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,12 +1,48 @@
-import Projects from "@/components/Projects";
+// // pages/projects.js
+// 'use client';
 
-const ProjectsPage = () => {
+// import { useEffect, useState } from "react";
+// import Parser from 'rss-parser';
+
+// const parser = new Parser();
+
+// export async function getServerSideProps() {
+//     try {
+//         const response = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@spencer.attick');
+//         const data = await response.json();
+//         console.log('DATA: ', data);
+//         return { props: { initialData: data.items } };
+//     } catch (error) {
+//         console.log(error);
+//         return { props: { initialData: [] } };
+//     }
+// }
+
+// const ProjectsPage = ({ initialData }) => {
+//     const [projects, setProjects] = useState(initialData);
+
+//     useEffect(() => {
+//         setProjects(initialData);
+//     }, [initialData]);
+
+//     return (
+//         <div>
+//             <h1>PROJECTS</h1>
+//             <ul>
+//                 {projects.map((project, index) => (
+//                     <li key={index}>{project.title}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default ProjectsPage;
+
+const Projects = () => {
     return (
-        <>
-            <Projects />
-        </>
+        <h1>PROJECTS</h1>
     )
-
 }
 
-export default ProjectsPage;
+export default Projects;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,39 @@
-const Projects = () => {
+// pages/projects.js
+'use client';
+
+import { useEffect, useState } from "react";
+import Parser from 'rss-parser';
+
+const parser = new Parser();
+
+export async function getServerSideProps() {
+    try {
+        const response = await fetch('https://medium.com/feed/@spencer.attick');
+        const feedXML = await response.text();
+        const feed = await parser.parseString(feedXML);
+        console.log('FEED ', feed)
+        return { props: { initialData: feed.items } };
+    } catch (error) {
+        console.error(error);
+        return { props: { initialData: [] } }; // Return empty data in case of error
+    }
+}
+
+const Projects = ({ initialData }) => {
+    const [projects, setProjects] = useState(initialData);
+
+    useEffect(() => {
+        setProjects(initialData);
+    }, [initialData]);
+
     return (
         <div>
             <h1>PROJECTS</h1>
+            <ul>
+                {/* {projects.map((project, index) => (
+                    <li key={index}>{project.title}</li>
+                ))} */}
+            </ul>
         </div>
     )
 }
