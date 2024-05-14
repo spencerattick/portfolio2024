827d49351095accfac5b39d97471ee5048908d85
Spencer Attick <spencer.attick@gmail.com>
2024-05-14 18:14:44 -0400

    add styling to techStack

--- pages/techStack/index.jsx
+++ pages/techStack/index.jsx
@@ -6,17 +6,17 @@ export default function TechStack() {
     <RootLayout>
       <div className="flex flex-col justify-center items-center min-h-screen bg-black">
         <h1 className="text-teal-600 font-bold text-4xl my-10 mt-12">Tech Stack</h1>
-        <div className="flex flex-col items-start">
+        <div className="flex flex-col items-center">
           {techStack.map((tech, index) => {
             return (
-              <div key={index} className="p-4 flex items-center">
-                <span className="mr-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" className="h-9 w-9">
+              <div key={index} className="p-4 flex flex-col items-center">
+                <span className="mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" className="h-10 w-10">
                     <path d={tech.icon}/>
                   </svg>
                 </span>
                 <span className="text-red-500">{tech.name}</span>
-                <span className="bg-yellow-400 w-10 h-8 ml-5"></span>
+                <span className={`bg-yellow-400 w-${tech.level} h-6 ml-5`}></span>
               </div>
             );
           })}
