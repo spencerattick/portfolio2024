d30792911e870f6d523304277a0e8bb4f932ea44
Spencer Attick <spencer.attick@gmail.com>
2024-05-14 17:55:42 -0400

    add styling to techStack

--- pages/techStack/index.jsx
+++ pages/techStack/index.jsx
@@ -4,25 +4,23 @@ import techStack from "../../public/techStack.json";
 export default function TechStack() {
   return (
     <RootLayout>
-      <div className="flex flex-col justify-center items-center h-screen bg-black">
-            <h1 className="text-teal-600 font-bold text-4xl my-10 mt-12">Tech Stack</h1>
-            <ul className="">
-                {
-                    techStack.map((tech, index) => {
-                        console.log('TECH ', tech.name)
-                        return (
-                            <>
-                                <li key={index} className="text-red-500">{tech.name}</li>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white">
-                                    <path d={tech.icon}/>
-                                </svg>
-                            </>
-                          
-                        )
-                    })
-                }
-            </ul>
-       
+      <div className="flex flex-col justify-center items-center min-h-screen bg-black">
+        <h1 className="text-teal-600 font-bold text-4xl my-10 mt-12">Tech Stack</h1>
+        <div className="flex flex-col items-start">
+          {techStack.map((tech, index) => {
+            return (
+              <div key={index} className="p-4 flex items-center">
+                <span className="mr-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="white" className="h-9 w-9">
+                    <path d={tech.icon}/>
+                  </svg>
+                </span>
+                <span className="text-red-500">{tech.name}</span>
+                <span className="bg-yellow-400 w-10 h-8 ml-5"></span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </RootLayout>
   );
