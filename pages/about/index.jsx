8f5ecb09d824d57c96078312b434997577cdaa0b
Spencer Attick <spencer.attick@gmail.com>
2024-05-17 15:35:44 -0400

    render book titles and images in About

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -12,47 +12,52 @@ const parser = new Parser();
 export async function getServerSideProps() {
     try {
         const response = await fetch('https://www.goodreads.com/user/updates_rss/104822881');
-        const data = await response.json();
-        return { props: { initialData: data } };
+        const feedXML = await response.text();
+        const feed = await parser.parseString(feedXML);
+        // console.log('DATA: ', feed)
+        return { props: { initialData: feed } };
     } catch (error) {
         console.error(error);
         return { props: { initialData: [] } }; // Return empty data in case of error
     }
 }
 
-const getDate = time => {
-  const parsedTimestamp = time.toString().slice(0, -3);
-  const date = new Date(parsedTimestamp * 1000).toDateString();
-  const splitDate = date.split(' ');
-  return `${splitDate[1]} ${splitDate[2]} ${splitDate[3]}`;
-} 
+// const getDate = time => {
+//   console.log('TIME: ', time)
+//   // const parsedTimestamp = time.toString().slice(0, -3);
+//   // const date = new Date(parsedTimestamp * 1000).toDateString();
+//   // const splitDate = date.split(' ');
+//   // return `${splitDate[1]} ${splitDate[2]} ${splitDate[3]}`;
+// } 
 
-const textToDisplay = item => {
-  let text;
-  if (item.title.includes('wants to read')) {
-    text = `added to list ${getDate(item.created)}`
-  } else if (item.title.includes('finished reading') || item.title.includes('has read') || item.title.includes('Spencer added')) {
-    text = `finished on ${getDate(item.created)}`
-  } else if (item.title.includes('started reading') || item.title.includes('currently reading')) {
-    text = `started reading on ${getDate(item.created)}`
-  }
-  return text;
-}
+// const textToDisplay = item => {
+//   let text;
+//   if (item.title.includes('wants to read')) {
+//     text = `added to list ${getDate(item.created)}`
+//   } else if (item.title.includes('finished reading') || item.title.includes('has read') || item.title.includes('Spencer added')) {
+//     text = `finished on ${getDate(item.created)}`
+//   } else if (item.title.includes('started reading') || item.title.includes('currently reading')) {
+//     text = `started reading on ${getDate(item.created)}`
+//   }
+//   return text;
+// }
 
 
 const getBookImg = book => {
-  const description = book.description;
-  const start = description.indexOf('books/') + 5;
-  const end =  description.indexOf('.jpg');
+  const content = book.content;
+  console.log('DESCRIPTION: ', content)
+  const start = content.indexOf('src="');
+  const end =  content.indexOf('.jpg');
 
-  const parsedImg = description.substring(start, end);
+  const parsedImg = content.substring(start + 5, end + 4);
+  console.log('PARSED: ', parsedImg)
 
-  const firstId = parsedImg.split('/')[1];
-  const secondId = parsedImg.split('/')[2].split('.')[0];
+  // const firstId = parsedImg.split('/')[1];
+  // const secondId = parsedImg.split('/')[2].split('.')[0];
 
-  const imgURL = `https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${firstId}/${secondId}.jpg`;
+  // const imgURL = `https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${firstId}/${secondId}.jpg`;
 
-  return imgURL;
+  return parsedImg;
 }
 
 const getBookTitle = title => {
@@ -76,22 +81,25 @@ const About = ({ initialData }) => {
           <div id="goodreads-container">
               {goodReadsFeed.items ? (
                   goodReadsFeed.items.map((book, key) => {
+                    console.log('BOOOK: ', book)
                       const bookImg = getBookImg(book);
                       const bookTitle = getBookTitle(book.title);
-                      const text = textToDisplay(book);
+                      console.log('IMAGE: ', bookImg)
+                      // const text = textToDisplay(book);
                       return (
-                      <div key={key} className="book-div">
-                          <a href={book.link} target="_blank">
-                              <img src={bookImg} alt={bookTitle} />
-                              <p>{text}</p>
-                          </a>
-                        
-                      </div>
+                        <div key={key} className="book-div">
+                            {/* <a href={book.link} target="_blank">
+                                <img src={bookImg} alt={bookTitle} />
+                                <p>{text}</p>
+                            </a> */}
+                            <p>{bookTitle}</p>
+                            <Image src={bookImg} width={100} height={100} alt={bookTitle}></Image>
+                          
+                        </div>
                       );
                   })
                   ) : (
                   <div>
-                    {console.log('FEEEEED: ', goodReadsFeed)}
                     It looks like something went wrong. You can see what I'm reading here: <a href="https://www.goodreads.com/user/show/104822881-spencer-attick">Goodreads Feed</a>.
                   </div>
               )}
