c8999681519c75ce98cf12577216fdbab9821676
Spencer Attick <spencer.attick@gmail.com>
2024-05-23 20:44:29 -0400

    add styling to AboutMe

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -3,15 +3,15 @@ import aboutMe from "../public/aboutMe.png";
 
 const AboutMe = () => {
     return (
-        <div className="flex justify-center items-center border border-black h-full text-center mt-20">
-          <div className="flex-col justify-center items-center border border-black h-full text-center">
-            <h1 className="font-bold text-2xl">Hi, I'm Spencer! I'm a full stack software engineer who is constantly learning new things.</h1>
-            <p>
+        <div className="flex justify-center items-center h-full m-20">
+          <div className="flex-col justify-center items-center h-full p-20">
+            <h1 className="font-bold text-4xl">Hi, I'm Spencer! I'm a full stack software engineer who is constantly learning new things.</h1>
+            <p className="text-xl">
               After pivoting from education into tech, I've spent my career leaning into my curiousity by way of creative problem solving and continually seeking out new projects. I'm incredibly passionate about software as a tool for social good. 
             </p>
           </div>
           <div>
-            <Image src={aboutMe} width={200} height={200} alt="Spencer hiking in the mountains"/>
+            <Image src={aboutMe} width={800} height={800} alt="Spencer hiking in the mountains" className="rounded-2xl rotate-3" />
           </div>
         </div>
    
