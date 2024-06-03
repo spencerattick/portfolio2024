c10dd4a6c7ec16d71fe7b061d3c6f850680aa7e0
Spencer Attick <spencer.attick@gmail.com>
2024-06-03 11:43:17 -0400

    add Contact image

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,30 +1,41 @@
 import RootLayout from "../../app/layout";
 import contact from "../../public/contact.json";
+import Image from "next/image";
+import contactPhoto from "../../public/contact.png";
 
 export default function Contact() {
   return (
     <RootLayout>
-      <div className="flex-col text-center">
+      <div className="flex flex-col text-center h-screen">
         <h1 className="text-black font-bold text-4xl my-10 mt-12">CONTACT ME</h1>
-        <div className='flex-col justify-end p-4'>
-            {contact.map((contactObj, index) => {
-                return (
-                    <a href={contactObj.link} target="_blank" key={index}>
-                    <div className="relative group">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='w-8 h-8 cursor-pointer text-black hover:text-red-500 hover:h-10 hover:w-10 ease-in duration-200 border border-red-400'fill="currentColor" > 
-                            <path d={contactObj.icon}/>
-                        </svg>
-                        <div className="absolute bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2">
-                            {contactObj.method}
-                        </div>
-                    </div>
-                    </a>
-                )
-            
-            })}
+        <div className="flex justify-center space-x-32">
+          <Image src={contactPhoto} width={300} height={300} alt="Spencer hiking in Colorado"></Image>
+          <div className="flex flex-col p-4 items-center">
+            {contact.map((contactObj, index) => (
+              <a
+                href={contactObj.link}
+                target="_blank"
+                key={index}
+                className="flex items-center mb-4 w-full max-w-xs group"
+              >
+                <div className="flex items-center w-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    className="w-8 h-8 cursor-pointer text-black group-hover:text-red-500"
+                    fill="currentColor"
+                  >
+                    <path d={contactObj.icon} />
+                  </svg>
+                  <div className="ml-4 text-left group-hover:text-red-500">
+                    {contactObj.method}
+                  </div>
+                </div>
+              </a>
+            ))}
+        </div>
         </div>
       </div>
-      
     </RootLayout>
   );
 }
